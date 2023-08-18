45004e371ab2f1e7cc4920ad7e1e9d6c5486582a
diggi404 <dbackson1@gmail.com>
2023-08-18 18:26:12 +0000

    don't display error

--- menu/smtpBrute/smtp.go
+++ menu/smtpBrute/smtp.go
@@ -19,7 +19,6 @@ func ConnectSMTP(credentials []string, testEmail string) ([]string, error) {
 	mailer.SetBody("text/plain", emailBody)
 	err := dialer.DialAndSend(mailer)
 	if err != nil {
-		fmt.Printf("err: %v\n", err)
 		return nil, err
 	}
 	credentials = append(credentials, "587")
