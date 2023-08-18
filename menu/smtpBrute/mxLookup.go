cd35d4cfa82b75c6fa2c21f1d34e85c60ead98b5
diggi404 <dbackson1@gmail.com>
2023-08-18 19:09:09 +0000

    fixed spliting bug in mxlookup

--- menu/smtpBrute/mxLookup.go
+++ menu/smtpBrute/mxLookup.go
@@ -1,6 +1,7 @@
 package smtpbrute
 
 import (
+	"errors"
 	"net"
 	"strings"
 )
@@ -8,6 +9,10 @@ import (
 func LookupDomain(creds []string) ([]string, error) {
 	username, password := creds[0], creds[1]
 	domainSplit := strings.Split(username, "@")
+	if len(domainSplit) != 2 {
+		err := errors.New("invalid email format")
+		return nil, err
+	}
 	domain := domainSplit[1]
 	record, err := net.LookupMX(domain)
 	if err != nil {
